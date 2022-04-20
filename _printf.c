a0feb081b10dd14bc581d2f9efc57add5d274532
mathaijohn0githinji <johngithinji083@gmail.com>
2022-04-20 16:09:25 +0300

    function that produces output according to a format

--- _printf.c
+++ _printf.c
@@ -1,4 +1,7 @@
-#include "main.h"
+#include <stdarg.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
 
 /**
  * _printf - printf function
@@ -7,38 +10,34 @@
  */
 int _printf(const char *format, ...)
 {
-	int (*pfunc)(va_list, flags_t *);
-	const char *p;
-	va_list arguments;
-	flags_t flags = {0, 0, 0};
+	va_list list;
+	int i = 0; j = 0;
+	char *dest;
 
-	register int count = 0;
-
-	va_start(arguments, format);
-	if (!format || (format[0] == '%' && !format[1]))
-		return (-1);
-	if (format[0] == '%' && format[1] == ' ' && !format[2])
-		return (-1);
-	for (p = format; *p; p++)
+	dest = malloc(sizeof(char) * 1500);
+	if (dest == NULL)
+		return (1);
+	va_start(list, format);
+	while(format[i] != '\0')
 	{
-		if (*p == '%')
+		if (format == '%')
 		{
-			p++;
-			if (*p == '%')
+			i++;
+			if (format[i] == 'c')
 			{
-				count += _putchar('%');
-				continue;
+				dest[j] = (char)va_arg(list, int);
+				j++;
 			}
-			while (get_flag(*p, &flags))
-				p++;
-			pfunc = get_print(*p);
-			count += (pfunc)
-				? pfunc(arguments, &flags)
-				: _printf("%%%c", *p);
-		} else
-			count += _putchar(*p);
+		}
+		else
+		{
+			dest[j] = format[i];
+			j++;
+		}
+		i++;
 	}
-	_putchar(-1);
-	va_end(arguments);
-	return (count);
+	write(1, dest, i++);
+	va_end(list);
+	free(list);
+	return (i);
 }
